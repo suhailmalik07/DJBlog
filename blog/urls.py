8c371103b787152123aff1f4d426d1e21d14f5b1
suhailmalik07 <suhailmalik8057@gmail.com>
2019-07-15 20:00:37 +0530

    initial release

---
+++ blog/urls.py
@@ -0,0 +1,27 @@
+"""django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .views import HomeView, AboutView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-post'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new', PostCreateView.as_view(), name='post-create'),
+    path('about/', AboutView.as_view(), name='blog-about'),
+]
---
+++ users/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
+
+# Create your views here.
+
+
+class RegisterView(View):
+    template_name = 'users/register.html'
+
+    def get(self, request, *args, **kwargs):
+        form_class = UserRegisterForm()
+        return render(request, self.template_name, {'form': form_class})
+
+    def post(self, request, *args, **kwargs):
+        form_class = UserRegisterForm(request.POST)
+        if form_class.is_valid():
+            form_class.save()
+            messages.success(
+                request, f"Your account has beed created now you can Login!")
+            return redirect('login')
+        return render(request, self.template_name, {'form': form_class})
+
+
+class ProfileView(LoginRequiredMixin, View):
+    template_name = 'users/profile.html'
+
+    def get(self, request):
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        context = {
+            'u_form': u_form,
+            'p_form': p_form,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        context = {
+            'u_form': u_form,
+            'p_form': p_form,
+        }
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Your Profile updated!")
+            return redirect('profile')
+        return render(request, self.template_name, context)
