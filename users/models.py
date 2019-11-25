79a5c8cb72af3d33140fb77259171c73b2857c03
suhail malik <suhailmalik8057@gmail.com>
2019-11-25 14:28:33 +0530

    fixes for default profile on user creation

--- users/models.py
+++ users/models.py
@@ -1,18 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from PIL import Image
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, )
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{self.user.username} profile'
+        return f'{self.user} profile'
 
-    def save(self):
-        super().save()
-
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
