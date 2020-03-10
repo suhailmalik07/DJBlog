a275e12faec9c66ae4555f18c5c9c80c74ca6011
Suhail Malik <devmsuhail@gmail.com>
2020-03-10 12:12:30 +0530

    Changed model to store time with date when creating user post

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateField(auto_now_add=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
